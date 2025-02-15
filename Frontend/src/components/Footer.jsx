f32932252798603456be9e1e8d4b951f6b44ee33
Tamilarasan M <140683736+TAMILARASAN2308@users.noreply.github.com>
2025-02-16 01:05:19 +0530

    Responsive Business Website

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,109 @@
+
+function Footer() {
+  return (
+    <>
+    <section className="hidden md:flex flex-col  items-center bg-[#1D2122] w-full h-[491px] ">
+        <div className="flex flex-col justify-between w-[1202px] h-[253px] mt-[160px]">
+        <div className="flex justify-between text-white">
+            <div className="flex flex-col gap-8">
+                <p className="text-[16px] font-semibold">Social Links</p>
+                <div className="flex gap-2">
+                    <img src="/images/facebook.png" className="w-[40px] h-[40px]"/>
+                    <img src="/images/twit.png" className="w-[40px] h-[40px]"/>
+                    <img src="/images/instagram.png" className="w-[40px] h-[40px]"/>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <p className="text-[16px] font-semibold">Menu</p>
+                <div className="text-[16px] font-normal text-[#A3A3A3] flex-col gap-4">
+                    <p>About</p>
+                    <p>Services</p>
+                    <p>Blog</p>
+                    <p>Contact</p>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <p className="text-[16px] font-semibold">Service</p>
+                <div className="text-[16px] font-normal text-[#A3A3A3] flex-col gap-4">
+                    <p>Design</p>
+                    <p>Development</p>
+                    <p>Marketing</p>
+                    <p>See More</p>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <p className="text-[16px] font-semibold">Subscribe</p>
+                <p className="text-[16px] font-normal text-[#A3A3A3] w-[251px]">leap into electronic remaining essentially unchanged.</p>
+                <div className="flex gap-2">
+                <input name='email' className='rounded-[4px] p-4 h-[36px] w-[155px] text-[12px] font-medium text-[#A3A3A3] bg-[#1D2122] border border-white' type='email' placeholder='Enter your email'/>
+                <button className=" bg-green-custom px-1 py-1 text-white rounded-[4px] w-[73px] h-[36px]">Send</button> 
+                </div>
+            </div>
+        </div>
+        <div className="flex justify-center">
+        <div className="w-[1170px] h-[1px] bg-[#F5F5F5] mix-blend-overlay"></div>
+        </div>
+        <div className="flex justify-between">
+           <p className="text-[16px] font-normal text-[#A3A3A3]">Copyright © 2023 pmcmarkagency. All Rights Reserved.</p>
+           <div className="flex justify-between gap-24">
+            <p className="text-[16px] font-normal text-[#A3A3A3]">Terms of Use</p>
+            <p className="text-[16px] font-normal text-[#A3A3A3]">Privacy Policy</p>
+           </div>
+        </div>
+        </div>
+    </section>
+    <section className="bg-[#1D2122] min-w-[360px] h-[888px] flex flex-col items-center font-poppins-font gap-4 py-14 md:hidden ">
+        <div className="flex flex-col justify-between gap-8 text-white">
+            <div className="flex flex-col gap-8">
+                <p className="text-[16px] font-semibold">Social Links</p>
+                <div className="flex gap-2">
+                    <img src="/images/facebook.png" className="w-[40px] h-[40px]"/>
+                    <img src="/images/twit.png" className="w-[40px] h-[40px]"/>
+                    <img src="/images/instagram.png" className="w-[40px] h-[40px]"/>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <p className="text-[16px] font-semibold">Menu</p>
+                <div className="text-[16px] font-normal text-[#A3A3A3] flex-col gap-4">
+                    <p>About</p>
+                    <p>Services</p>
+                    <p>Blog</p>
+                    <p>Contact</p>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <p className="text-[16px] font-semibold">Service</p>
+                <div className="text-[16px] font-normal text-[#A3A3A3] flex-col gap-4">
+                    <p>Design</p>
+                    <p>Development</p>
+                    <p>Marketing</p>
+                    <p>See More</p>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <p className="text-[16px] font-semibold">Subscribe</p>
+                <p className="text-[16px] font-normal text-[#A3A3A3] w-[251px]">leap into electronic remaining essentially unchanged.</p>
+                <div className="flex flex-col gap-2">
+                <input name='email' className='rounded-[4px] p-4 h-[36px] w-[155px] text-[12px] font-medium text-[#A3A3A3] bg-[#1D2122] border border-white' type='email' placeholder='Enter the email'/>
+                <button className=" bg-green-custom px-1 py-1 text-white rounded-[4px] w-[73px] h-[36px]">Send</button> 
+                </div>
+            </div>
+        </div>
+        <div className="flex flex-col justify-center">
+        <div className="w-[313px] h-[1px] bg-[#F5F5F5] mix-blend-overlay"></div>
+        </div>
+        <div className="flex flex-col pl-12 gap-2">
+        <div className="flex flex-col gap-2">
+            <p className="text-[16px] font-normal text-[#A3A3A3]">Terms of Use</p>
+            <p className="text-[16px] font-normal text-[#A3A3A3]">Privacy Policy</p>
+        </div>
+        <div className="flex flex-col">
+           <p className="text-[16px]  w-[298px] h-[48px] font-normal text-[#A3A3A3]">Copyright © 2023 pmcmarkagency. All Rights Reserved.</p>
+        </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Footer
---
+++ Frontend/src/components/TeamSwiper.jsx
@@ -0,0 +1,103 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+
+const teamMembers = [
+  { id: 1, name: "John Smith", role: "CEO", image: "/images/T1.png" },
+  { id: 2, name: "Siman Adams", role: "CTO", image: "/images/T2.png" },
+  { id: 3, name: "Paul Jones", role: "Design Lead", image: "/images/T3.png" },
+  { id: 4, name: "Sara Hardin", role: "Project Manager", image: "/images/T4.png" },
+  { id: 5, name: "John Smith", role: "CEO", image: "/images/T1.png" },
+  { id: 6, name: "Siman Adams", role: "CTO", image: "/images/T2.png" },
+  { id: 7, name: "Paul Jones", role: "Design Lead", image: "/images/T3.png"  },
+  { id: 8, name: "Sara Hardin", role: "Project Manager", image: "/images/T4.png" },
+  { id: 9, name: "John Smith", role: "CEO", image: "/images/T1.png" },
+  { id: 10,name: "Siman Adams", role: "CTO", image: "/images/T2.png" },
+  { id: 11, name: "Paul Jones", role: "Design Lead", image: "/images/T3.png"  },
+  { id: 12, name: "Sara Hardin", role: "Project Manager", image: "/images/T4.png"  },
+];
+
+const teamMembers2 = [
+  { id: 1, name: "John Smith", role: "CEO", image: "/images/T1.png" },
+  { id: 2, name: "Jane Doe", role: "CTO, Company", image: "/images/T2.png" },
+  { id: 3, name: "Michael Smith", role: "CFO, Company", image: "/images/T3.png" },
+  { id: 4, name: "Emily Johnson", role: "COO, Company", image: "/images/T4.png" },
+];
+
+function TeamSwiper() {
+  return (
+    <>
+    <section className="hidden md:flex justify-center mt-[54px] font-poppins-font">
+      <div className="w-[1204px] h-[542px] flex flex-col gap-6">
+        <h2 className="text-[48px] font-semibold text-[#04293C] leading-[64px] w-[373px] h-[64px]">Meet Our Team</h2>
+        
+        <div className="relative">
+          <Swiper
+            modules={[Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+            slidesPerGroup={4}
+            pagination={{
+              clickable: true,
+              el: ".custom-pagination ",
+            }}
+            loop={false} 
+          >
+            {teamMembers.map((member) => (
+              <SwiperSlide key={member.id} className="flex justify-center items-center">
+                <div className="flex flex-col items-center justify-center gap-2 w-[277px] h-[322px] bg-white rounded-[10px] p-5 font-poppins-font shadow-lg border border-gray-300">
+                  <img src={member.image} className="w-[157px] h-[157px] rounded-full mb-2" alt={`${member.name} Image`} />
+                  <h6 className="text-[24px] font-medium leading-none text-[#1D2130]">{member.name}</h6>
+                  <p className="text-[16px] font-medium  leading-none text-[#04293C] opacity-[0.7]">{member.role}</p>
+                  <div className="flex gap-2 mt-2">
+                    <img src="/images/I1.png" className="w-[20px] h-[20px]" alt="Icon 1" />
+                    <img src="/images/I2.png" className="w-[20px] h-[20px]" alt="Icon 2" />
+                    <img src="/images/I3.png" className="w-[20px] h-[20px]" alt="Icon 3" />
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="custom-pagination "></div>
+        </div>
+      </div>
+    </section>
+    <section className="md:hidden flex justify-center mt-6 font-poppins-font">
+    <div className="w-full max-w-xs flex flex-col gap-6">
+      <h2 className="text-[32px] font-semibold text-[#04293C] h-[40px] w-[249px]">Meet Our Team</h2>
+      
+      <div className="relative">
+        <Swiper
+          modules={[Pagination]}
+          spaceBetween={10}
+          slidesPerView={1}
+          slidesPerGroup={1}
+          pagination={{ clickable: true, el: ".custom-pagination-mobile" }}
+          loop={false}
+        >
+          {teamMembers2.map((member) => (
+            <SwiperSlide key={member.id} className="flex justify-center items-center">
+              <div className="flex flex-col items-center justify-center gap-2 w-[277px] h-[322px] bg-white rounded-lg p-5 shadow-lg border border-gray-300 font-poppins-font">
+                <img src={member.image} className="w-[157px] h-[157px] rounded-full mb-4" alt={`${member.name} Image`} />
+                <h6 className="text-[24px] font-medium text-[#04293C] leading-none">{member.name}</h6>
+                <p className="text-[16px] font-medium text-[#04293C] leading-none opacity-[0.7]">{member.role}</p>
+                <div className="flex gap-2 mt-2">
+                  <img src="/images/I1.png" className="w-[20px] h-[20px]" alt="Icon 1" />
+                  <img src="/images/I2.png" className="w-[20px] h-[20px]" alt="Icon 2" />
+                  <img src="/images/I3.png" className="w-[20px] h-[20px]" alt="Icon 3" />
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="custom-pagination-mobile flex justify-center mt-4"></div>
+      </div>
+    </div>
+  </section>
+  </>
+  );
+}
+
+export default TeamSwiper;
